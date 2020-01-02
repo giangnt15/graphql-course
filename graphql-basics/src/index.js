4d6987fea30b40c2b7dfe3834d6e6e1ea8af200e
giangnt15 <giangqwerty69@gmail.com>
2020-01-03 00:12:48 +0700

    init

---
+++ graphql-basics/src/index.js
@@ -0,0 +1,85 @@
+// import message, {name as myName,getGreeting} from './myModules';
+// import add, {subtract} from './myOtherModules';
+
+// console.log(add(1,2),subtract(3,4))
+
+import {GraphQLServer} from 'graphql-yoga';
+
+const typeDefs = `
+    type User{
+        id: ID!
+        name: String!
+        email: String!
+        age: Int!
+    }
+
+    type Post{
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+    }
+
+    type Query{
+        me: User!
+        add(a: Float!, b: Float!): Float!
+        post: Post!
+        getPostById(id: ID!): Post!
+        posts: [Post!]!
+    }
+
+`
+
+let posts = [{
+    id: "post1",
+    title: "My post",
+    body: "Hello",
+    published: false
+},{
+    id: "post2",
+    title: "My post 2",
+    body: "Hello again!",
+    published: true
+}]
+
+const resolvers = {
+    Query: {
+        me(){
+            return {
+                id: "123",
+                name: "Giang",
+                email: "giangqwerty69@gmail.com"
+            }
+        },
+        post(){
+            return {
+                id: "post1",
+                title: "My post",
+                body: "Hello",
+                published: false
+            }
+        },
+        add(parent,args,ctx,info){
+            console.log('parent',parent);
+            if (args.a!=null&&args.b!=null){
+                return args.a+args.b;
+            }else {
+                throw new Error("Please supply both numbers");
+            }
+        },
+        getPostById(parent,args,ctx,info){
+            console.log(info)
+            return posts.find(item=>item.id===args.id);
+        },
+        posts(){
+            return posts;
+        },
+    }
+}
+const server = new GraphQLServer({
+     typeDefs,
+     resolvers
+})
+
+server.start({},()=>console.log("Running on port 4000"))
+
---
+++ graphql-basics/src/myModules.js
@@ -0,0 +1,10 @@
+const message = "Giang oi";
+
+const name = "giang";
+export {message as default, 
+    name
+};
+
+export const getGreeting = ()=> "Welcome";
+
+// export default message;
