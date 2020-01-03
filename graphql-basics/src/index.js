fcb216aee2b87ef571f7cb0db61a69f919403a18
giangnt15 <giangqwerty69@gmail.com>
2020-01-03 23:39:59 +0700

    Sua file index va graphql-memo

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -6,19 +6,16 @@
 import {
     GraphQLServer
 } from 'graphql-yoga';
+import uuidv4 from 'uuid/v4';
 
 const typeDefs = `
     type User{
         id: ID!
         name: String!
         email: String!
-<<<<<<< HEAD
         age: Int
         posts: [Post!]!
-=======
-        grades: [Float!]!
-        age: Int!
->>>>>>> e71c1f276c3dff2da3da8d4442b74783f47ea0e8
+        comments: [Comment!]!
     }
 
     type Post{
@@ -27,22 +24,31 @@ const typeDefs = `
         body: String!
         published: Boolean!
         author: User!
+        comments: [Comment!]!
     }
 
+    type Comment{
+        id: ID!
+        text: String!
+        author: User!
+        post: Post!
+    }
 
     type Query{
         me: User!
-<<<<<<< HEAD
         users(query: String): [User!]! 
-        add(a: Float!, b: Float!): Float!
-=======
         add(numbers: [Float!]!): Float!
->>>>>>> e71c1f276c3dff2da3da8d4442b74783f47ea0e8
         post: Post!
         getPostById(id: ID!): Post!
         posts(query: String): [Post!]!
-    }
 
+        comments: [Comment!]!
+    }
+    type Mutation{
+        createUser(name: String!, email: String!, age: Int): User!
+        createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+        createComment(text: String!, author: ID!, post: ID!): Comment!
+    }
 `
 
 let posts = [{
@@ -50,26 +56,31 @@ let posts = [{
     title: "My post",
     body: "Hello",
     published: false,
-    author: 'user1'
+    author: 'user1',
+    comments: ['1','3']
 }, {
     id: "post2",
     title: "My post 2",
     body: "Hello again!",
     published: true,
-    author: 'user2'
+    author: 'user2',
+    comments: ['2']
 
 }, {
     id: "post3",
     title: "Hello",
     body: "Hello again!",
     published: true,
-    author: 'user3'
+    author: 'user3',
+    comments: ['4']
+
 }, {
     id: "post4",
     title: "Hello",
     body: "Hello again!",
     published: true,
-    author: 'user1'
+    author: 'user1',
+    comments: []
 }]
 
 let users = [{
@@ -77,21 +88,50 @@ let users = [{
     name: "Nguyen Giang",
     email: "giangnt15@gmail.com",
     age: 21,
-    posts: ['post1','post4']
+    posts: ['post1','post4'],
+    comments: ['1','2']
 }, {
     id: "user2",
     name: "Nguyen Son",
     email: "sonnt13@gmail.com",
     age: 16,
-    posts: ['post2']
+    posts: ['post2'],
+    comments: ['3']
+
 }, {
     id: "user3",
     name: "Nguyen Thanh",
     email: "thanhnt20@gmail.com",
     age: 16,
-    posts: ['post3']
+    posts: ['post3'],
+    comments: ['4']
+
 }]
 
+let comments = [{
+    id: '1',
+    text: 'This is a comment',
+    author: 'user1',
+    post: 'post1'
+},{
+    id: '2',
+    text: 'This is a comment 2',
+    author: 'user1',
+    post: 'post2'
+
+},{
+    id: '3',
+    text: 'This is a comment 3',
+    author: 'user2',
+    post: 'post1'
+
+},{
+    id: '4',
+    text: 'This is a comment 4',
+    author: 'user3',
+    post: 'post3'
+
+}]
     
 const resolvers = {
     User: {
@@ -100,22 +140,80 @@ const resolvers = {
         // },
         posts(parent,args,ctx,info){
             return posts.filter(post=>parent.posts.findIndex(p=> p===post.id)>=0);
+        },
+        comments(parent,args,ctx,info){
+            return comments.filter(comment=>comment.author===parent.id);
         }
     },
     Post: {
         author(parent,args,ctx,info){
             return users.find(user=> user.id===parent.author);
 
+        },
+        comments(parent,args,ctx,info){
+            return comments.filter(comment=>comment.post===parent.id);
         }
     },
-    Post: {
-        body(parent,args){
-            console.log("parent",parent)
-            console.log("args",args)
-            return "This is a body"
+    Comment:{
+        author(parent,args,ctx,info){
+            return users.find(user=>user.id===parent.author);
+        },
+        post(parent,args,ctx,info){
+            return posts.find(post=>post.id===parent.post);
+        }
+    },
+    Mutation: {
+        createUser(parent,args,ctx,info){
+            const {name,email,age} = args;
+            const isMatch = users.some(user=> user.email===email);
+            if (!isMatch){
+                let user = {id: uuidv4(), name, email,age}
+                users.push(user)
+                return user;
+            }
+            throw new Error('Email taken');
+
+        },
+        createPost(parent,args,ctx,info){
+            const {title,body,published,author} = args;
+            const userExisted = users.some(user=>user.id===author);
+            if (userExisted){
+                const post = {
+                    title,
+                    body,
+                    published,
+                    author,
+                    id: uuidv4()
+                }
+                posts.push(post);
+                return post;
+            }
+            throw new Error("User not existed");
+        },
+        createComment(parent,args,ctx,info){
+            const {text,author,post} = args;
+
+            const userExisted = users.some(user=>user.id===author);
+            if (!userExisted) throw new Error("User not existed");
+            const post1 = posts.find(p=>p.id===post);
+            if (!post1) throw new Error("Post not existed");
+            if (!post1.published) throw new Error("Post not published");
+            const comment = {
+                id: uuidv4(),
+                text,
+                author,
+                post
+            }
+
+            comments.push(comment);
+            return comment;
+
         }
     },
     Query: {
+        comments(parent,args,ctx,info){
+            return comments;
+        },
         users(parent, {
             query
         }, ctx, info) {
