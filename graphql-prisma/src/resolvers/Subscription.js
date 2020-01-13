69610a95db24ac52ca55665798a028e77655aeba
Nguyen Truong Giang <aquarius123654@outlook.com>
2020-01-13 11:09:09 +0700

    Sua file graphql-memo

--- graphql-prisma/src/resolvers/Subscription.js
+++ graphql-prisma/src/resolvers/Subscription.js
@@ -2,38 +2,56 @@ const Subscription = {
     // subscription resolver for a subscription operation is an object containing
     // subscribe() method running every time a client try to subscribe to it
     count: {
-        subscribe(parent, args, {db, pubSub, prisma}, info){
+        subscribe(parent, args, {
+            db,
+            pubSub,
+            prisma
+        }, info) {
             let count = 0;
-            setInterval(()=>{
+            setInterval(() => {
                 count++;
-                pubSub.publish('count',{count/* match up with the subscription name (here is count)*/})//publish the change to chanel "count"
-            },1000);
+                pubSub.publish('count', {
+                    count /* match up with the subscription name (here is count)*/
+                }) //publish the change to chanel "count"
+            }, 1000);
             //setup the chanel named "count"
             //here we are not return an integer
             return pubSub.asyncIterator('count')
         }
     },
     comment: {
-        async subscribe(parent,{postId},{db,pubSub,prisma},info){
-            return prisma.subscription.comment({where: {
-                node: {
-                    post: {
-                        id: postId
+        async subscribe(parent, {
+            postId
+        }, {
+            db,
+            pubSub,
+            prisma
+        }, info) {
+            return prisma.subscription.comment({
+                where: {
+                    node: {
+                        post: {
+                            id: postId
+                        }
                     }
                 }
-            }},info)
+            }, info)
         }
-       
+
     },
     post: {
-        subscribe(parent, args, {db,pubSub, prisma}, info){
+        subscribe(parent, args, {
+            db,
+            pubSub,
+            prisma
+        }, info) {
             return prisma.subscription.post({
                 where: {
                     node: {
                         published: true
                     }
                 }
-            },info);
+            }, info);
         }
     }
 }
